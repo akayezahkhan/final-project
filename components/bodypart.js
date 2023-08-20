7ec2da0db41d9ab273461e4c706032803c5141d2
Ayezah Maryam <ayeza.maryam123@gmail.com>
2023-08-20 23:55:46 +0500

    duplicated

--- components/bodypart.js
+++ components/bodypart.js
@@ -57,6 +57,43 @@ function Bodypart() {
           ))}
         </ul>
       </div>
+      <div className="m-2 border-2 border-orange-600">
+        <h2 className=" text-lg">ToDo List</h2>
+        <div className=" flex items-center space-x-2">
+          <input
+            type="text"
+            placeholder="Enter Your Todo Task"
+            className="input input-info "
+            value={inputValue}
+            onChange={handleInputValue}
+          />
+          <button
+            onClick={handleAddTodo}
+            className="p-2 text-white rounded-lg bg-green-600"
+          >
+            Add
+          </button>
+        </div>
+        <ul className="my-4">
+          {todo.map((todoText, index) => (
+            <li
+              key={index}
+              className=" flex justify-between items-center my-1 "
+            >
+              <div className=" flex items-center space-x-2">
+                {" "}
+                <input type="checkbox" className="" /> <span>{todoText}</span>{" "}
+              </div>
+              <button
+                onClick={() => handleDeleteTodo(index)}
+                className="p-2 text-white rounded-lg bg-red-600"
+              >
+                Delete
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
