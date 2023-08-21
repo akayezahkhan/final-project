0af8585e04833dfbab2d5ff0ffce6f7dd6f2e648
Ayezah Maryam <ayeza.maryam123@gmail.com>
2023-08-21 11:46:24 +0500

    456

--- app/signup/page.js
+++ app/signup/page.js
@@ -3,7 +3,7 @@ import SignupForm from "../../components/SignupForm";
 
 export default function SignUp() {
   return (
-    <div className="h-screen w-screen overflow-scroll">
+    <div className="h-screen w-screen overflow-hidden">
       <SignupForm />
     </div>
   );
