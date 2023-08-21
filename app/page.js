54b71ef6c856e5ad73c7b9b298b3c44b9b1f4158
Ayezah Maryam <ayeza.maryam123@gmail.com>
2023-08-21 11:42:58 +0500

    overflow hidden

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import LoginForm from "../components/LoginForm";
 
 export default function Home() {
   return (
-    <div className="h-screen w-screen overflow-scroll">
+    <div className="h-screen w-screen overflow-hidden">
       <LoginForm />
     </div>
   );
