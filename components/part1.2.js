c676ff363e1ae2b7442d6c888b323bcd1f635170
Ayezah Maryam <ayeza.maryam123@gmail.com>
2023-08-25 00:38:22 +0500

    update part1_2

--- components/part1.2.js
+++ components/part1.2.js
@@ -1,123 +1,75 @@
-import { useState } from "react";
+"use client";
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faLocation,
+  faToggleOn,
+  faToggleOff,
+} from "@fortawesome/free-solid-svg-icons";
 
-function Part1_2() {
-  const [todo, setTodo] = useState([]);
-  const [inputValue, setInputValue] = useState("");
-
-  const handleInputValue = (e) => {
-    setInputValue(e.target.value);
+export default function Part1_2() {
+  const [menuIcon, setMenuIcon] = useState(faToggleOff);
+  const toggle = () => {
+    setMenuIcon(menuIcon == faToggleOff ? faToggleOn : faToggleOff);
   };
-
-  const handleAddTodo = () => {
-    setTodo([...todo, inputValue]);
-    setInputValue("");
-  };
-
-  const handleDeleteTodo = (index) => {
-    const newTodo = todo.filter((_, i) => i !== index);
-    setTodo(newTodo);
-  };
-
-  const [notes, setnotes] = useState([]);
-  const [inputnotesValue, setInputnotesValue] = useState("");
-
-  const handleInputnotesValue = (e) => {
-    setInputnotesValue(e.target.value);
-  };
-
-  const handleAddnotes = () => {
-    setnotes([...notes, inputnotesValue]);
-    setInputnotesValue("");
-  };
-
-  const handleDeletenotes = (index) => {
-    const newTodo = notes.filter((_, i) => i !== index);
-    setnotes(newTodo);
-  };
-
   return (
-    <div className="h-full flex flex-col rounded-lg p-1 ">
-      <div className="border-slate-100 border-2 rounded-lg h-full">
-        <div className="p-2 h-1/2 overflow-hidden">
-          <h2 className="text-md">To Do</h2>
-          <div className="flex justify-between">
-            <input
-              type="text"
-              placeholder="Enter task"
-              className="mr-3 rounded-lg px-2 min-w-[120px]"
-              value={inputValue}
-              onChange={handleInputValue}
-            />
-            <button
-              onClick={handleAddTodo}
-              className="p-2 text-white rounded-lg bg-green-600"
-            >
-              Add
-            </button>
-          </div>
-          <ul className="my-2">
-            {todo.map((list, index) => (
-              <li
-                key={index}
-                className=" flex justify-between items-center my-1 "
-              >
-                <div className=" flex items-center space-x-2">
-                  {" "}
-                  <input type="checkbox" className="" /> <span>{list}</span>{" "}
-                </div>
-                <button
-                  onClick={() => handleDeleteTodo(index)}
-                  className="text-red-600"
-                >
-                  - Delete
-                </button>
-              </li>
-            ))}
-          </ul>
-        </div>
-
-        <hr></hr>
-
-        <div className="p-2 h-1/2 overflow-hidden">
-          <h2 className="text-md">Notes</h2>
-          <div className="flex justify-between">
-            <input
-              type="text"
-              placeholder="Take note"
-              className="mr-3 rounded-lg px-2 min-w-[120px]"
-              value={inputnotesValue}
-              onChange={handleInputnotesValue}
-            />
-            <button
-              onClick={handleAddnotes}
-              className="p-2 text-white rounded-lg bg-green-600"
-            >
-              Add
-            </button>
-          </div>
-          <ul className="my-2">
-            {notes.map((notes, index) => (
-              <li
-                key={index}
-                className=" flex justify-between items-center my-1 "
-              >
-                <div className=" flex items-center space-x-2">
-                  {" "}
-                 <span>{notes}</span>{" "}
-                </div>
-                <button
-                  onClick={() => handleDeletenotes(index)}
-                  className="text-red-600"
-                >
-                  - Delete
-                </button>
-              </li>
-            ))}
-          </ul>
-        </div>
+    <div className=" mx-3 my-3 h-[64rem]">
+      <textarea
+        placeholder="Add Description"
+        className=" textarea textarea-info"
+      />
+      <div className=" flex space-x-3 items-center">
+        {" "}
+        <h1 className=" text-cyan-400"> Priority </h1>
+        <button className=" hover:scale-105 transition duration-200 my-3 bg-cyan-950 border-2 rounded-xl border-green-400 text-green-400 px-3 py-1 ">
+          Low
+        </button>
+        <button className=" hover:scale-105 transition duration-200 bg-cyan-950 border-2 rounded-xl border-yellow-400 text-yellow-400 px-3 py-1 ">
+          Medium
+        </button>
+        <button className=" hover:scale-105 transition duration-200 bg-cyan-950 border-2 rounded-xl border-red-400 text-red-400 px-3 py-1 ">
+          High
+        </button>
       </div>
+
+      <div className=" flex space-x-3 items-center">
+        {" "}
+        <h1 className=" text-cyan-400"> Repeat Every</h1>
+        <button className=" hover:scale-105 transition duration-200 my-3 bg-cyan-950 border-2 rounded-xl border-gray-100 text-gray-100 px-3 py-1 ">
+          2
+        </button>
+        <button className=" hover:scale-105 transition duration-200 my-3 bg-cyan-950 border-2 rounded-xl border-gray-100 text-gray-100 px-3 py-1 ">
+          weeks
+        </button>
+      </div>
+
+      <div className=" flex items-center space-x-3 my-3">
+        <h1 className=" text-cyan-400">Add Attachment</h1>
+        <input type="file" className="  file-input-info" />
+      </div>
+
+      <div className=" flex items-center space-x-3 my-3">
+        <h1 className=" text-cyan-400">Add Location</h1>
+        <FontAwesomeIcon
+          icon={faLocation}
+          className=" hover:scale-105 transition duration-150 cursor-pointer h-5 w-5"
+        />
+      </div>
+
+      <div className=" flex items-center space-x-3 my-3">
+        <h1 className=" text-cyan-400">Make Habbit</h1>
+        {/* <FontAwesomeIcon icon={menuIcon} onClick={toggle} className=' h-5 w-5 hover:scale-105 transition duration-150 cursor-pointer' /> */}
+        <input type="checkbox" className=" toggle toggle-info" />
+      </div>
+
+      <textarea
+        placeholder="Add Tags like Work, Exercise etc"
+        className=" textarea-info"
+      />
+
+      <button className=" hover:scale-105 transition duration-200 bg-black text-white rounded-xl px-2 py-1 float-right my-72">
+        Save Task
+      </button>
     </div>
   );
 }
-
-export default Part1_2;
